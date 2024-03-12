6082c9e0e1cb9e1357a60aab9e19eec692cc3fe1
GAEL RG <grodriguezg22@fpcoruna.afundacion.org>
2024-03-12 10:23:42 +0100

    RecyclerView Creado (Ejercicio 2)

---
+++ PokeRecuperacionT2/app/src/main/java/com/example/pokerecuperaciont2/Adaptador.java
@@ -0,0 +1,24 @@
+package com.example.pokerecuperaciont2;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adaptador extends RecyclerView.Adapter<Celda> {
+    @NonNull
+    @Override
+    public Celda onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Celda holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ PokeRecuperacionT2/app/src/main/java/com/example/pokerecuperaciont2/Celda.java
@@ -0,0 +1,17 @@
+package com.example.pokerecuperaciont2;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Celda extends RecyclerView.ViewHolder {
+
+    public Celda(@NonNull View itemView) {
+        super(itemView);
+    }
+}
+
